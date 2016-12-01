88e5f3a4954c0df85ec9fce050245515f83acce8
liujun <lj90214@gmail.com>
2016-12-01 14:29:32 +0800

    修改多对多为默认的表名

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $fillable = ['key', 'name'];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
 }
--- database/migrations/2016_11_30_162322_create_user_role_table.php
+++ database/migrations/2016_11_30_162322_create_user_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserRolesTable extends Migration
+class CreateUserRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_roles', function(Blueprint $table) {
+        Schema::create('user_role', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('role_id');
@@ -28,6 +28,6 @@ class CreateUserRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('user_role');
     }
 }
