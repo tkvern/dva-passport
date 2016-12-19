c0276cf61f72811a68c0265f7a714ea8f057bd98
tkvern <tkvern@qq.com>
2016-12-19 16:32:49 +0800

    重命名password

---
+++ resources/assets/ant_passport/src/routes/Password.jsx
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'dva';
+
+import MainLayout from '../components/layout/MainLayout';
+import UserPassword from '../components/user/UserPassword.jsx';
+
+
+function Password({ location, dispatch, auth}) {
+  const {
+    user,
+  } = auth;
+
+  const userPasswordProps = {
+    user: user,
+    onUpdate(data) {
+      dispatch({
+        type: 'auth/password',
+        payload: data,
+      })
+    }
+  };
+
+  return (
+    <MainLayout>
+      <UserPassword { ...userPasswordProps } />
+    </MainLayout>
+  );
+}
+
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Password);
